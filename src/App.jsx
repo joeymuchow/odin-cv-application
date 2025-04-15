411feff67237a00149110e61d33a7adc05ef90e1
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-04-15 11:33:00 -0700

    Create Button and Input components and state vars
    Made components for buttons and inputs that are used by other
    components. I also created state variables for handling the infomation
    of each section that is passed into their corresponding component. Also
    created a state variable to handle showing the inputs or cv view.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
+import GeneralInformation from './components/GeneralInformation'
+import EducationalExperience from './components/EducationalExperience'
+import PracticalExperience from './components/PracticalExperience'
+import Button from './components/Button'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [generalInfo, setGeneralInfo] = useState({})
+  const [educationalExp, setEducationalExp] = useState({})
+  const [practicalExp, setPracticalExp] = useState({})
+  const [showCV, setShowCV] = useState(false)
 
   // Should all the components below be surrounded by a form?
   // As in this file has a form element surrounding all components
@@ -10,31 +17,25 @@ function App() {
   // Educational experience
   // Practical experience
 
+  // make a visual component that uses info from above components to create a resume
+
   // There will be a submit button for taking all the entered info and displaying it in a view
   // There should be either one edit button for the whole form or one for each section. I'm leaning toward one edit button for everything
   // Clicking edit will show the components for entering info again, with their current values filled in
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      {
+        showCV ?
+          <div>
+            <Button text='Edit' onClick={() => setShowCV(false)} />
+          </div> :
+          <>
+            <GeneralInformation generalInfo={generalInfo} setGeneralInfo={setGeneralInfo} />
+            <EducationalExperience educationalExp={educationalExp} setEducationalExp={setEducationalExp} />
+            <PracticalExperience practicalExp={practicalExp} setPracticalExp={setPracticalExp} />
+            <Button text='Submit' onClick={() => setShowCV(true)} />
+          </>
+      }
     </>
   )
 }
--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -1,9 +1,20 @@
-function GeneralInformation() {
+import Input from "./Input"
+
+function GeneralInformation({ generalInfo, setGeneralInfo }) {
     // name, two inputs for first/last or one input for the whole name?
     // Should I make components for name, email, and phone?
 
     return (
-        <></>
+        <>
+            <Input id="first-name" label="First name" onChange={(e) => setGeneralInfo({
+                ...generalInfo,
+                firstName: e.target.value,
+            })} />
+            <Input id="last-name" label="Last name" onChange={(e) => setGeneralInfo({
+                ...generalInfo,
+                lastName: e.target.value,
+            })} />
+        </>
     )
 }
 
